3c0836d26f3ac0e5769f2df7b7f93040be944011
R3noir <dogeplayer101@gmail.com>
2024-01-17 00:51:31 +0800

    Login page rewritten

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+require('dotenv').config()
+const supabase = require('@supabase/supabase-js')
+
 const path = require('path');
 const app = express();
+const Database = supabase.createClient(process.env.URL, process.env.KEY)
+app.use(express.json());
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,3 +26,13 @@ app.get('/resetpassword', (req, res) => {
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+//Authentication
+app.post('/auth', async (req, res) => {
+  Database.auth.signOut();
+  const { email, password } = req.body;
+  const { data, error } = await Database.auth.signInWithPassword({ email, password });
+
+  if (error) return res.status(401).json({ error: error.message });
+  return res.status(200).json({ data });
+});
--- public/js/Login.js
+++ public/js/Login.js
@@ -1,15 +1,25 @@
-document.getElementById('togglePassword').addEventListener('click', function (e) {
-    const passwordInput = document.getElementById('password');
-    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordInput.setAttribute('type', type);
-    
-    // Get the i element
-    const icon = this.querySelector('i');
-    
-    // Toggle the text content of the i element
-    if (icon.textContent === 'visibility') {
-        icon.textContent = 'visibility_off';
+const togglePassword = document.getElementById('togglePassword');
+const togglePasswordIcon = togglePassword.children[0];
+const snackbar = document.getElementById('snackbar');
+const SnackBarDelayMs = 3000;
+
+//Events
+togglePassword.addEventListener('click', function () {
+    var passwordInput = document.getElementById('password');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        togglePasswordIcon.innerHTML = 'visibility';
     } else {
-        icon.textContent = 'visibility';
+        passwordInput.type = 'password';
+        togglePasswordIcon.innerHTML = 'visibility_off';
     }
+
+    if(snackbar.className == 'show') {
+        return;
+    }
+snackbar.className = 'show';
+// After 3 seconds, remove the show class from DIV
+setTimeout(function(){ snackbar.className = snackbar.className.replace('show', 'hide'); }, SnackBarDelayMs);
 });
+
+// Show the snackbar
